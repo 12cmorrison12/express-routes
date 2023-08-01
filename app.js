42f2217868e1b1466a01f443baeb7fe47af738fb
Carver Morrison <12cmorrison12@gmail.com>
2023-08-01 13:43:13 -0600

    added functions file to help with app routes

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/mean', function(req, res, next) {
+  
+})
+
+app.listen(3000, function() {
+    console.log(`Server starting on port 3000`);
+  });
